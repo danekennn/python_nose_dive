1c1a24e3f31633d21f1c6f775cdbfcaceb3c1326
danekennn <73371489+danekennn@users.noreply.github.com>
2020-11-21 10:33:57 +0900

    .
    .

--- hello_world_pygame.py
+++ hello_world_pygame.py
@@ -7,7 +7,7 @@ height = 400
 white = (255, 255, 255)
 black = (  0,   0,   0)
 fps = 30
-stride = 5
+stride = 10
 x = 0
 y = 0
 
@@ -18,7 +18,7 @@ displaysurf = pygame.display.set_mode((width, height), 0, 32)
 clock = pygame.time.Clock()
 
 gulimfont = pygame.font.SysFont('굴림', 70)
-helloworld = gulimfont.render('Hello, world!', 1, black)
+helloworld = gulimfont.render('Hello, world!', 1, (200,180,0))
 
 hellorect = helloworld.get_rect()
 hellorect.center = (width / 2, height / 2)
@@ -30,15 +30,23 @@ while True:
             pygame.quit()
             sys.exit()
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        x += stride
-    elif keys[pygame.K_a]:
-        x -= stride
-    elif keys[pygame.K_w]:
-        y -= stride
-    elif keys[pygame.K_s]:
+    if keys[pygame.K_RIGHT]:
+        if hellorect.right < width:
+            x += stride
+    elif keys[pygame.K_LEFT]:
+        if hellorect.left > 0:
+            x -= stride
+    if keys[pygame.K_UP]:
+        if hellorect.top > 0:
+            y -= 4 * stride
+    elif keys[pygame.K_DOWN]:
+        if hellorect.bottom < height:
+            y += stride
+
+    if hellorect.bottom < height:
         y += stride
-    displaysurf.fill(white)
+
+    displaysurf.fill(black)
     hellorect.center = (width/2 + x, height /2 + y)
     displaysurf.blit(helloworld, hellorect)
 
